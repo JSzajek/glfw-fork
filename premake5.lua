06980bd594ff42b74f849640fa37218e3f66f4d7
jszajek <jszajek2@gmail.com>
2023-07-09 11:10:30 -0500

    Updated bin and bin-int to Binaries and Intermediates.

--- premake5.lua
+++ premake5.lua
@@ -4,8 +4,8 @@ project "GLFW"
 
 	staticruntime "on"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("Binaries/" .. outputdir .. "/%{prj.name}")
+	objdir ("Intermediates/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
